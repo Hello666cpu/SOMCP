3446238a83d96948f0bf1a3ac7776fb2ce7054f8
sjlz <15884976704@163.com>
2026-07-19 19:52:59 +0800

    Release SOMCP 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.soreverse.mcp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         externalNativeBuild {
             cmake {
